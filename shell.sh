8772357c8bb44cd25fa655f46ab61d89088324d8
lzjwlt <lzj@lzjwlt.cn>
2019-04-28 13:35:15 +0000

    fix: remove exit

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ ln -s server.vimrc ~/.vimrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-exit && zsh
+zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions --depth=1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git --depth=1 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --depth=1
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf --depth=1
